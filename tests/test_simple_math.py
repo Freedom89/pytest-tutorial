dbf4d339fdff42956b1282528ca6c96e3ea722f9
yixiang.low <yixiang.low@traveloka.com>
2020-12-08 23:01:26 +0800

    fix minor typo

--- tests/test_simple_math.py
+++ tests/test_simple_math.py
@@ -2,9 +2,9 @@ from src import simple_math
 
 
 def test_factorial():
-    assert simple_math.factorial(3) == 6, "response is correct"
+    assert simple_math.factorial(3) == 6, "response is incorrect"
 
 
 def tet_factorial():
     # this will not get test
-    assert simple_math.factorial(10) == 6, "response is correct"
+    assert simple_math.factorial(10) == 6, "response is incorrect"
