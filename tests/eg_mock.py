c47fc44a30ef35df580b9302cf9f676385becd5c
Low Yi Xiang <yxlowx@gmail.com>
2021-08-02 00:43:07 +0800

    Revert "rename mock to demo mock"

--- tests/eg_mock.py
+++ tests/eg_mock.py
@@ -1,19 +1,19 @@
 import pytest
-import src.demo_mock
-from src.demo_mock import add_minus_10, get_constant
+import src.mock
+from src.mock import add_minus_10, get_constant
 
 # Constants
 
 
 def test_replace_constant(mocker):
-    mocker.patch.object(src.demo_mock, "CONSTANT", 123)
+    mocker.patch.object(src.mock, "CONSTANT", 123)
     expected = 246
     actual = get_constant()  # you would expect 200
     assert actual == expected, "something went wrong"
 
 
 def test_replace_function(mocker):
-    mocker.patch("src.demo_mock.add", return_value=200)
+    mocker.patch("src.mock.add", return_value=200)
     actual = add_minus_10(10, 20)  # you would expect 20
     expected = 190
     assert actual == expected, "something went wrong"
